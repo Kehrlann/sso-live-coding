9da52355bd809dc90974d569ace8ea3fbce42836
Daniel Garnier-Moiroux <dgarnier@vmware.com>
2023-06-08 22:28:11 +0200

    python: base application, hardcoded user

---
+++ python/app.py
@@ -0,0 +1,39 @@
+from uuid import uuid4
+from flask import Flask, render_template, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = uuid4().bytes
+
+
+@app.route("/")
+def hello():
+    if "username" in session:
+        print(session["attributes"])
+        return render_template(
+            "authenticated.html",
+            username=session["username"],
+            attributes=session["attributes"],
+        )
+    return render_template("anonymous.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    session["username"] = "Daniel"
+    session["attributes"] = {
+        "firstName": "Daniel",
+        "lastName": "Garnier-Moiroux",
+        "company": "VMware",
+        "userType": "hardcoded",
+    }
+    return redirect(url_for("hello"))
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    session.clear()
+    return redirect(url_for("hello"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
