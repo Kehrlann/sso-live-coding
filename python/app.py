c037cf0b0dd63d42ce15c11731a1a238f25cbe2f
Daniel Garnier-Moiroux <dgarnier@vmware.com>
2023-06-09 12:24:57 +0200

    python: sso with google

--- python/app.py
+++ python/app.py
@@ -1,8 +1,19 @@
+import json
 from uuid import uuid4
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, request
+from requests import post
+from urllib.parse import urlencode
+from base64 import urlsafe_b64decode
+from dotenv import dotenv_values
+
+config = dotenv_values("../.env")
 
 app = Flask(__name__)
 app.secret_key = uuid4().bytes
+
+client_id = config["SSO_CLIENT_ID"]
+client_secret = config["SSO_CLIENT_SECRET"]
+redirect_uri = "http://localhost:5000/oauth2/callback"
 
 
 @app.route("/")
@@ -14,7 +25,18 @@ def hello():
             username=session["username"],
             attributes=session["attributes"],
         )
-    return render_template("anonymous.html")
+
+    encodedParams = urlencode(
+        {
+            "response_type": "code",
+            "client_id": client_id,
+            "redirect_uri": redirect_uri,
+            "scope": "openid profile email",
+        }
+    )
+
+    login_uri = "https://accounts.google.com/o/oauth2/v2/auth?" + encodedParams
+    return render_template("anonymous.html", login_uri=login_uri)
 
 
 @app.route("/login", methods=["POST"])
@@ -32,6 +54,26 @@ def login():
 @app.route("/logout", methods=["POST"])
 def logout():
     session.clear()
+    return redirect(url_for("hello"))
+
+
+@app.route("/oauth2/callback")
+def callback():
+    code = request.args.get("code")
+    resp = post(
+        "https://oauth2.googleapis.com/token",
+        data={
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": redirect_uri,
+        },
+        auth=(client_id, client_secret),
+    ).json()
+    payload = resp["id_token"].split(".")[1]
+    decoded = urlsafe_b64decode(payload)
+    claims = json.loads(decoded)
+    session["username"] = claims["email"]
+    session["attributes"] = claims
     return redirect(url_for("hello"))
 
 
