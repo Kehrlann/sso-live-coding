559dee1b3908cb93de355d7bddb6a6764222cb76
Daniel Garnier-Moiroux <dgarnier@vmware.com>
2023-10-01 18:35:48 +0200

    java: prep for demo

--- java/src/main/java/wf/garnier/sso/demo/HelloController.java
+++ java/src/main/java/wf/garnier/sso/demo/HelloController.java
@@ -1,41 +1,20 @@
 package wf.garnier.sso.demo;
 
-import java.io.IOException;
-import java.util.Base64;
+import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 public class HelloController {
 
-    public static final String AUTHORIZATION_SUCCESS_PATH = "/oauth2/callback";
-
-    static final String REDIRECT_URI = "http://localhost:8080" + AUTHORIZATION_SUCCESS_PATH;
-
     private final RestClient restClient;
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-
-    @Value("${sso.client_id}")
-    private String clientId;
-
-    @Value("${sso.client_secret}")
-    private String clientSecret;
-
-    private static final String authorizationUri = "https://accounts.google.com/o/oauth2/v2/auth";
-
-    private static final String tokenUri = "https://oauth2.googleapis.com/token";
 
     public HelloController(RestClient.Builder builder) {
         this.restClient = builder.build();
@@ -49,12 +28,6 @@ public class HelloController {
             model.addAttribute("attributes", session.getAttribute("attributes"));
             return "authenticated";
         } else {
-            var loginUri = UriComponentsBuilder.fromHttpUrl(authorizationUri)
-                    .queryParam("client_id", clientId)
-                    .queryParam("redirect_uri", REDIRECT_URI)
-                    .queryParam("scope", "openid profile email")
-                    .queryParam("response_type", "code");
-            model.addAttribute("loginUri", loginUri.toUriString());
             return "anonymous";
         }
     }
@@ -79,49 +52,24 @@ public class HelloController {
     public String logout(HttpServletRequest request) {
         var session = request.getSession();
         if (session != null) {
-            session.removeAttribute("username");
-            session.removeAttribute("attributes");
+            session.invalidate();
         }
         return "redirect:/";
     }
 
-    @GetMapping(AUTHORIZATION_SUCCESS_PATH)
-    public String authorized(@RequestParam("code") String code, HttpServletRequest request) throws IOException {
-        System.out.println("Got a code: " + code);
-        var response = this.restClient
-                .post()
-                .uri(tokenUri)
-                .header("Authorization", "Basic " + getCredentials())
-                .body(
-                        Map.of(
-                                "redirect_uri", REDIRECT_URI,
-                                "grant_type", "authorization_code",
-                                "code", code
-                        )
-                )
-                .retrieve()
-                .body(String.class);
-        var body = this.objectMapper.readValue(response, Map.class);
-        System.out.println(body.get("id_token"));
+    @GetMapping("/conferences")
+    public String conferences(Model model, HttpServletRequest request) {
+        try {
+            var conferences = this.restClient.get()
+                    .uri("http://localhost:8081/conferences?userId={id}", "-1")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .body(List.class);
+            model.addAttribute("conferences", conferences);
+        } catch (Exception e) {
+            model.addAttribute("error", "Error while getting conferences: " + e.getMessage());
+        }
 
-        var idToken = decodeIdToken((String) body.get("id_token"));
-        System.out.println(idToken);
-
-        var session = request.getSession(true);
-        session.setAttribute("username", idToken.get("email"));
-        request.getSession().setAttribute("attributes", idToken);
-
-        return "redirect:/";
-    }
-
-    private Map<String, Object> decodeIdToken(String idToken) throws IOException {
-        var parts = idToken.split("\\.");
-        var body = Base64.getUrlDecoder().decode(parts[1]);
-        return objectMapper.readValue(body, Map.class);
-    }
-
-    private String getCredentials() {
-        var credsString = "%s:%s".formatted(clientId, clientSecret);
-        return Base64.getUrlEncoder().encodeToString(credsString.getBytes());
+        return "conferences";
     }
 }
