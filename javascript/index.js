6f5fa71b467cae335066963221b6b465786bf48c
Daniel Garnier-Moiroux <dgarnier@vmware.com>
2023-10-02 14:10:17 +0200

    javascript: add resource-server support

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid')
 const express = require('express')
 const app = express()
 
@@ -15,11 +16,12 @@ const redirect_uri = "http://localhost:3000/oauth2/callback"
 app.get('/', (req, res) => {
   session = req.session
   if (!session.username) {
-    const loginUri = new URL("https://accounts.google.com/o/oauth2/v2/auth")
+    const loginUri = new URL("https://dev-51438889.okta.com/oauth2/default/v1/authorize")
     loginUri.searchParams.append("client_id", client_id)
     loginUri.searchParams.append("redirect_uri", redirect_uri)
     loginUri.searchParams.append("response_type", "code")
-    loginUri.searchParams.append("scope", "openid email profile")
+    loginUri.searchParams.append("scope", "openid email profile test.scope")
+    loginUri.searchParams.append("state", uuid.v4())
     res.render('anonymous', { loginUri })
   } else {
     res.render('authenticated', {
@@ -43,19 +45,16 @@ app.post('/login', (req, res) => {
 
 app.post('/logout', (req, res) => {
   session = req.session
-  session.username = null
-  session.attributes = null
-  res.redirect('/')
+  session.destroy(() => res.redirect('/'))
 })
 
 app.get('/oauth2/callback', (req, res) => {
   const code = req.query.code
   console.log(code)
-  fetch("https://oauth2.googleapis.com/token", {
+  fetch("https://dev-51438889.okta.com/oauth2/default/v1/token", {
     method: "POST",
     body: new URLSearchParams({
       code,
-      client_id,
       redirect_uri,
       grant_type: "authorization_code"
     }),
@@ -66,6 +65,7 @@ app.get('/oauth2/callback', (req, res) => {
   })
     .then(r => r.json())
     .then(r => {
+      console.log(r)
       const id_token = r.id_token
       const payload = id_token.split(".")[1]
       const parsed = JSON.parse(atob(payload))
@@ -73,8 +73,39 @@ app.get('/oauth2/callback', (req, res) => {
       const session = req.session
       session.username = parsed.email
       session.attributes = parsed
+      session.access_token = r.access_token
       res.redirect("/")
     })
+})
+
+app.get("/conferences", (req, res) => {
+  fetch('http://localhost:8081/conferences', {
+    headers: {
+      "authorization": `Bearer ${req.session.access_token}`
+    }
+  })
+    .then(r => {
+      if (r.status == 200) {
+        return r.json()
+      } else {
+        return r.text().then(t => { throw new Error(t) })
+      }
+    })
+    .then(r =>
+      res.render('conferences', {
+        error: null,
+        conferences: r
+      })
+    )
+    .catch(e => {
+      console.log(e)
+      res.render('conferences', {
+        error: e,
+        conferences: []
+      })
+    }
+    )
+
 })
 
 
