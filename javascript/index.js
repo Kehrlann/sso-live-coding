9397d31e5de66dd69dab9885bfd66032cce0635c
Daniel Garnier-Moiroux <dgarnier@vmware.com>
2023-10-04 11:02:00 +0200

    javascript: prettier formatting

--- javascript/index.js
+++ javascript/index.js
@@ -1,114 +1,121 @@
-const uuid = require('uuid')
-const express = require('express')
-const app = express()
+const uuid = require("uuid");
+const express = require("express");
+const app = express();
 
-app.use(express.static('static'))
-app.use(require('express-session')({ secret: "sessionsecret", resave: true, saveUninitialized: true }))
+app.use(express.static("static"));
+app.use(
+  require("express-session")({
+    secret: "sessionsecret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.engine('handlebars', require('express-handlebars').engine())
-app.set('view engine', 'handlebars')
+app.engine("handlebars", require("express-handlebars").engine());
+app.set("view engine", "handlebars");
 
-const config = require("dotenv").config({ path: "../.env" }).parsed
-const client_id = config.SSO_CLIENT_ID
-const client_secret = config.SSO_CLIENT_SECRET
-const redirect_uri = "http://localhost:3000/oauth2/callback"
+const config = require("dotenv").config({ path: "../.env" }).parsed;
+const client_id = config.SSO_CLIENT_ID;
+const client_secret = config.SSO_CLIENT_SECRET;
+const redirect_uri = "http://localhost:3000/oauth2/callback";
 
-app.get('/', (req, res) => {
-  session = req.session
+app.get("/", (req, res) => {
+  session = req.session;
   if (!session.username) {
-    const loginUri = new URL("https://dev-51438889.okta.com/oauth2/default/v1/authorize")
-    loginUri.searchParams.append("client_id", client_id)
-    loginUri.searchParams.append("redirect_uri", redirect_uri)
-    loginUri.searchParams.append("response_type", "code")
-    loginUri.searchParams.append("scope", "openid email profile test.scope")
-    loginUri.searchParams.append("state", uuid.v4())
-    res.render('anonymous', { loginUri })
+    const loginUri = new URL(
+      "https://dev-51438889.okta.com/oauth2/default/v1/authorize"
+    );
+    loginUri.searchParams.append("client_id", client_id);
+    loginUri.searchParams.append("redirect_uri", redirect_uri);
+    loginUri.searchParams.append("response_type", "code");
+    loginUri.searchParams.append("scope", "openid email profile test.scope");
+    loginUri.searchParams.append("state", uuid.v4());
+    res.render("anonymous", { loginUri });
   } else {
-    res.render('authenticated', {
+    res.render("authenticated", {
       username: session.username,
-      attributes: session.attributes
-    })
+      attributes: session.attributes,
+    });
   }
-})
+});
 
-app.post('/login', (req, res) => {
-  session = req.session
-  session.username = "Daniel"
+app.post("/login", (req, res) => {
+  session = req.session;
+  session.username = "Daniel";
   session.attributes = {
-    "firstName": "Daniel",
-    "lastName": "Garnier-Moiroux",
-    "company": "VMware",
-    "userType": "hardcoded",
-  }
-  res.redirect('/')
-})
+    firstName: "Daniel",
+    lastName: "Garnier-Moiroux",
+    company: "VMware",
+    userType: "hardcoded",
+  };
+  res.redirect("/");
+});
 
-app.post('/logout', (req, res) => {
-  session = req.session
-  session.destroy(() => res.redirect('/'))
-})
+app.post("/logout", (req, res) => {
+  session = req.session;
+  session.destroy(() => res.redirect("/"));
+});
 
-app.get('/oauth2/callback', (req, res) => {
-  const code = req.query.code
-  console.log(code)
+app.get("/oauth2/callback", (req, res) => {
+  const code = req.query.code;
+  console.log(code);
   fetch("https://dev-51438889.okta.com/oauth2/default/v1/token", {
     method: "POST",
     body: new URLSearchParams({
       code,
       redirect_uri,
-      grant_type: "authorization_code"
+      grant_type: "authorization_code",
     }),
     headers: {
       authorization: `Basic ${btoa(client_id + ":" + client_secret)}`,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
   })
-    .then(r => r.json())
-    .then(r => {
-      console.log(r)
-      const id_token = r.id_token
-      const payload = id_token.split(".")[1]
-      const parsed = JSON.parse(atob(payload))
-      console.log(parsed)
-      const session = req.session
-      session.username = parsed.email
-      session.attributes = parsed
-      session.access_token = r.access_token
-      res.redirect("/")
-    })
-})
+    .then((r) => r.json())
+    .then((r) => {
+      console.log(r);
+      const id_token = r.id_token;
+      const payload = id_token.split(".")[1];
+      const parsed = JSON.parse(atob(payload));
+      console.log(parsed);
+      const session = req.session;
+      session.username = parsed.email;
+      session.attributes = parsed;
+      session.access_token = r.access_token;
+      res.redirect("/");
+    });
+});
 
 app.get("/conferences", (req, res) => {
-  fetch('http://localhost:8081/conferences', {
+  fetch("http://localhost:8081/conferences", {
     headers: {
-      "authorization": `Bearer ${req.session.access_token}`
-    }
+      authorization: `Bearer ${req.session.access_token}`,
+    },
   })
-    .then(r => {
+    .then((r) => {
       if (r.status == 200) {
-        return r.json()
+        return r.json();
       } else {
-        return r.text().then(t => { throw new Error(t) })
+        return r.text().then((t) => {
+          throw new Error(t);
+        });
       }
     })
-    .then(r =>
-      res.render('conferences', {
+    .then((r) =>
+      res.render("conferences", {
         error: null,
-        conferences: r
+        conferences: r,
       })
     )
-    .catch(e => {
-      console.log(e)
-      res.render('conferences', {
+    .catch((e) => {
+      console.log(e);
+      res.render("conferences", {
         error: e,
-        conferences: []
-      })
-    }
-    )
-
-})
-
+        conferences: [],
+      });
+    });
+});
 
 app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`)
-})
+  console.log(`Example app listening on port ${3000}`);
+});
