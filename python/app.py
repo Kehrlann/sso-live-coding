d5668f9dbf7aa3bc69a3a52183dc84f5a8534f52
Daniel Garnier-Moiroux <git@garnier.wf>
2024-01-26 12:16:50 +0100

    python: prep for demo

--- python/app.py
+++ python/app.py
@@ -1,44 +1,20 @@
 import json
 from uuid import uuid4
 from flask import Flask, render_template, redirect, url_for, session, request
-from requests import post, get
-from urllib.parse import urlencode
-from base64 import urlsafe_b64decode
-from dotenv import dotenv_values
 from uuid import uuid4
-
-config = dotenv_values("../.env")
 
 app = Flask(__name__)
 app.secret_key = uuid4().bytes
 
-client_id = config["SSO_CLIENT_ID"]
-client_secret = config["SSO_CLIENT_SECRET"]
-redirect_uri = "http://localhost:5000/oauth2/callback"
-
-
 @app.route("/")
 def hello():
     if "username" in session:
-        print(session["attributes"])
         return render_template(
             "authenticated.html",
             username=session["username"],
             attributes=session["attributes"],
         )
-
-    encodedParams = urlencode(
-        {
-            "response_type": "code",
-            "client_id": client_id,
-            "redirect_uri": redirect_uri,
-            "scope": "openid profile email conference.list",
-            "state": uuid4()
-        }
-    )
-
-    login_uri = "https://dev-51438889.okta.com/oauth2/default/v1/authorize?" + encodedParams
-    return render_template("anonymous.html", login_uri=login_uri)
+    return render_template("anonymous.html")
 
 
 @app.route("/login", methods=["POST"])
@@ -56,28 +32,6 @@ def login():
 @app.route("/logout", methods=["POST"])
 def logout():
     session.clear()
-    return redirect(url_for("hello"))
-
-
-@app.route("/oauth2/callback")
-def callback():
-    code = request.args.get("code")
-    resp = post(
-        "https://dev-51438889.okta.com/oauth2/default/v1/token",
-        data={
-            "grant_type": "authorization_code",
-            "code": code,
-            "redirect_uri": redirect_uri,
-        },
-        auth=(client_id, client_secret),
-    ).json()
-    print(resp)
-    payload = resp["id_token"].split(".")[1]
-    decoded = urlsafe_b64decode(payload + "===")
-    claims = json.loads(decoded)
-    session["username"] = claims["email"]
-    session["attributes"] = claims
-    session["access_token"] = resp["access_token"]
     return redirect(url_for("hello"))
 
 @app.route("/conferences")
