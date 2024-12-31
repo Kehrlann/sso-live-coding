2c28b09bbd1584cb4b2c64f006676241c3121703
Daniel Garnier-Moiroux <git@garnier.wf>
2024-12-31 16:27:52 +0100

    Revert "java: use okta, use resource-server"
    This reverts commit 44fc5fa9ee68c8ca5eb453fadaecfce5f5e86960.

--- java/src/main/java/wf/garnier/sso/demo/HelloController.java
+++ java/src/main/java/wf/garnier/sso/demo/HelloController.java
@@ -2,19 +2,13 @@ package wf.garnier.sso.demo;
 
 import java.io.IOException;
 import java.util.Base64;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,13 +32,12 @@ public class HelloController {
     @Value("${SSO_CLIENT_SECRET}")
     private String clientSecret;
 
-    private static final String authorizationUri = "https://dev-51438889.okta.com/oauth2/default/v1/authorize";
+    private static final String authorizationUri = "https://accounts.google.com/o/oauth2/v2/auth";
 
-    private static final String tokenUri = "https://dev-51438889.okta.com/oauth2/default/v1/token";
+    private static final String tokenUri = "https://oauth2.googleapis.com/token";
 
-    public HelloController() {
-        this.restClient = RestClient.create();
-        this.objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    public HelloController(RestClient.Builder builder) {
+        this.restClient = builder.build();
     }
 
     @GetMapping("/")
@@ -58,9 +51,8 @@ public class HelloController {
             var loginUri = UriComponentsBuilder.fromHttpUrl(authorizationUri)
                     .queryParam("client_id", clientId)
                     .queryParam("redirect_uri", REDIRECT_URI)
-                    .queryParam("scope", "openid profile email conference.list")
-                    .queryParam("response_type", "code")
-                    .queryParam("state", UUID.randomUUID().toString());
+                    .queryParam("scope", "openid profile email")
+                    .queryParam("response_type", "code");
             model.addAttribute("loginUri", loginUri.toUriString());
             return "anonymous";
         }
@@ -86,65 +78,43 @@ public class HelloController {
     public String logout(HttpServletRequest request) {
         var session = request.getSession();
         if (session != null) {
-            session.invalidate();
+            session.removeAttribute("username");
+            session.removeAttribute("attributes");
         }
-        return "redirect:/";
-    }
-
-    @GetMapping("/conferences")
-    public String conferences(Model model, HttpServletRequest request) {
-        var session = request.getSession();
-        if (session != null && session.getAttribute("access_token") != null) {
-            try {
-                var conferences = this.restClient.get()
-                        .uri("http://localhost:8081/conferences")
-                        .header("authorization", "Bearer " + session.getAttribute("access_token"))
-                        .accept(MediaType.APPLICATION_JSON)
-                        .retrieve()
-                        .body(List.class);
-                model.addAttribute("conferences", conferences);
-            } catch (Exception e) {
-                model.addAttribute("error", "Error while getting conferences: " + e.getMessage());
-            }
-
-            return "conferences";
-        }
-
         return "redirect:/";
     }
 
     @GetMapping(AUTHORIZATION_SUCCESS_PATH)
     public String authorized(@RequestParam("code") String code, HttpServletRequest request) throws IOException {
         System.out.println("Got a code: " + code);
-        var payload = new LinkedMultiValueMap<>();
-        payload.add("redirect_uri", REDIRECT_URI);
-        payload.add("grant_type", "authorization_code");
-        payload.add("code", code);
-
         var response = this.restClient
                 .post()
                 .uri(tokenUri)
                 .header("Authorization", "Basic " + getCredentials())
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(payload)
+                .body(
+                        Map.of(
+                                "redirect_uri", REDIRECT_URI,
+                                "grant_type", "authorization_code",
+                                "code", code
+                        )
+                )
                 .retrieve()
                 .body(String.class);
-        var body = this.objectMapper.readValue(response, TokenReponse.class);
-        System.out.println(body.idToken());
+        var body = this.objectMapper.readValue(response, Map.class);
+        System.out.println(body.get("id_token"));
 
-        var idToken = decodeIdToken(body.idToken());
+        var idToken = decodeIdToken((String) body.get("id_token"));
         System.out.println(idToken);
 
         var session = request.getSession(true);
         session.setAttribute("username", idToken.get("email"));
-        session.setAttribute("attributes", idToken);
-        session.setAttribute("access_token", body.accessToken());
+        request.getSession().setAttribute("attributes", idToken);
 
         return "redirect:/";
     }
 
-    private Map<String, Object> decodeIdToken(String token) throws IOException {
-        var parts = token.split("\\.");
+    private Map<String, Object> decodeIdToken(String idToken) throws IOException {
+        var parts = idToken.split("\\.");
         var body = Base64.getUrlDecoder().decode(parts[1]);
         return objectMapper.readValue(body, Map.class);
     }
@@ -153,11 +123,4 @@ public class HelloController {
         var credsString = "%s:%s".formatted(clientId, clientSecret);
         return Base64.getUrlEncoder().encodeToString(credsString.getBytes());
     }
-
-    record TokenReponse(
-            @JsonProperty("id_token") String idToken,
-            @JsonProperty("access_token") String accessToken
-    ) {
-    }
-
 }
