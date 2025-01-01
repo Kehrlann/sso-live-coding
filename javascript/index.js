0ba082afe8c126644ec92aa8307d4526ee43a5e1
Daniel Garnier-Moiroux <git@garnier.wf>
2025-01-01 13:09:02 +0100

    Revert "javascript: add resource-server support"
    This reverts commit 6f5fa71b467cae335066963221b6b465786bf48c.

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,5 @@
-const uuid = require("uuid");
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 app.use(express.static("static"));
 app.use(
@@ -14,7 +13,7 @@ app.use(
 app.engine("handlebars", require("express-handlebars").engine());
 app.set("view engine", "handlebars");
 
-const config = require("dotenv").config({ path: "../.env" }).parsed;
+const config = require("dotenv").config({path: "../.env"}).parsed;
 const client_id = config.SSO_CLIENT_ID;
 const client_secret = config.SSO_CLIENT_SECRET;
 const redirect_uri = "http://localhost:3000/oauth2/callback";
@@ -22,15 +21,12 @@ const redirect_uri = "http://localhost:3000/oauth2/callback";
 app.get("/", (req, res) => {
   session = req.session;
   if (!session.username) {
-    const loginUri = new URL(
-      "https://dev-51438889.okta.com/oauth2/default/v1/authorize"
-    );
-    loginUri.searchParams.append("client_id", client_id);
-    loginUri.searchParams.append("redirect_uri", redirect_uri);
-    loginUri.searchParams.append("response_type", "code");
-    loginUri.searchParams.append("scope", "openid email profile conference.list");
-    loginUri.searchParams.append("state", uuid.v4());
-    res.render("anonymous", { loginUri });
+    const loginUri = new URL("https://accounts.google.com/o/oauth2/v2/auth")
+    loginUri.searchParams.append("client_id", client_id)
+    loginUri.searchParams.append("redirect_uri", redirect_uri)
+    loginUri.searchParams.append("response_type", "code")
+    loginUri.searchParams.append("scope", "openid email profile")
+    res.render('anonymous', {loginUri})
   } else {
     res.render("authenticated", {
       username: session.username,
@@ -51,18 +47,21 @@ app.post("/login", (req, res) => {
   res.redirect("/");
 });
 
-app.post("/logout", (req, res) => {
-  session = req.session;
-  session.destroy(() => res.redirect("/"));
-});
+app.post('/logout', (req, res) => {
+  session = req.session
+  session.username = null
+  session.attributes = null
+  res.redirect('/')
+})
 
-app.get("/oauth2/callback", (req, res) => {
-  const code = req.query.code;
-  console.log(code);
-  fetch("https://dev-51438889.okta.com/oauth2/default/v1/token", {
+app.get('/oauth2/callback', (req, res) => {
+  const code = req.query.code
+  console.log(code)
+  fetch("https://oauth2.googleapis.com/token", {
     method: "POST",
     body: new URLSearchParams({
       code,
+      client_id,
       redirect_uri,
       grant_type: "authorization_code",
     }),
@@ -71,50 +70,19 @@ app.get("/oauth2/callback", (req, res) => {
       "Content-Type": "application/x-www-form-urlencoded",
     },
   })
-    .then((r) => r.json())
-    .then((r) => {
-      console.log(r);
-      const id_token = r.id_token;
-      const payload = id_token.split(".")[1];
-      const parsed = JSON.parse(atob(payload));
-      console.log(parsed);
-      const session = req.session;
-      session.username = parsed.email;
-      session.attributes = parsed;
-      session.access_token = r.access_token;
-      res.redirect("/");
-    });
-});
-
-app.get("/conferences", (req, res) => {
-  fetch("http://localhost:8081/conferences", {
-    headers: {
-      authorization: `Bearer ${req.session.access_token}`,
-    },
-  })
-    .then((r) => {
-      if (r.status == 200) {
-        return r.json();
-      } else {
-        return r.text().then((t) => {
-          throw new Error(t);
-        });
-      }
+    .then(r => r.json())
+    .then(r => {
+      const id_token = r.id_token
+      const payload = id_token.split(".")[1]
+      const parsed = JSON.parse(atob(payload))
+      console.log(parsed)
+      const session = req.session
+      session.username = parsed.email
+      session.attributes = parsed
+      res.redirect("/")
     })
-    .then((r) =>
-      res.render("conferences", {
-        error: null,
-        conferences: r,
-      })
-    )
-    .catch((e) => {
-      console.log(e);
-      res.render("conferences", {
-        error: e,
-        conferences: [],
-      });
-    });
-});
+})
+
 
 app.listen(3000, () => {
   console.log(`Example app running at http://localhost:3000`);
