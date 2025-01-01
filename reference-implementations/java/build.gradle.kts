3a3d5afa2b8999ab54a24260e0cdca88f00748f7
Daniel Garnier-Moiroux <git@garnier.wf>
2025-01-01 12:06:17 +0100

    reference-java: bump to boot 3.4.1

--- reference-implementations/java/build.gradle.kts
+++ reference-implementations/java/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.1"
-    id("io.spring.dependency-management") version "1.1.3"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "wf.garnier"
