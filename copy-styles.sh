8ad00bd725102ce2fbad682457f410ec8caae92f
Daniel Garnier-Moiroux <git@garnier.wf>
2025-01-02 01:50:52 +0100

    build: add copy-styles.sh

---
+++ copy-styles.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cp style.css java/src/main/resources/static/style.css
+cp style.css python/static/style.css
+cp style.css javascript/static/style.css
+cp style.css reference-implementations/java/src/main/resources/static/style.css
+cp style.css reference-implementations/python/static/style.css
+cp style.css reference-implementations/javascript/static/style.css
