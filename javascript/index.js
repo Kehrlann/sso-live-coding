f9cfc1fea91488a906f8df59b6177fee70fa621e
Daniel Garnier-Moiroux <git@garnier.wf>
2025-01-14 13:17:59 +0100

    javascript: apply prettier

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 app.use(express.static("static"));
 app.use(
   require("express-session")({
     secret: "sessionsecret",
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: true
   })
 );
 
 app.engine("handlebars", require("express-handlebars").engine());
 app.set("view engine", "handlebars");
 
-const config = require("dotenv").config({path: "../.env"}).parsed;
+const config = require("dotenv").config({ path: "../.env" }).parsed;
 const client_id = config.SSO_CLIENT_ID;
 const client_secret = config.SSO_CLIENT_SECRET;
 const redirect_uri = "http://localhost:3000/oauth2/callback";
@@ -21,16 +21,16 @@ const redirect_uri = "http://localhost:3000/oauth2/callback";
 app.get("/", (req, res) => {
   session = req.session;
   if (!session.username) {
-    const loginUri = new URL("https://accounts.google.com/o/oauth2/v2/auth")
-    loginUri.searchParams.append("client_id", client_id)
-    loginUri.searchParams.append("redirect_uri", redirect_uri)
-    loginUri.searchParams.append("response_type", "code")
-    loginUri.searchParams.append("scope", "openid email profile")
-    res.render('anonymous', {loginUri})
+    const loginUri = new URL("https://accounts.google.com/o/oauth2/v2/auth");
+    loginUri.searchParams.append("client_id", client_id);
+    loginUri.searchParams.append("redirect_uri", redirect_uri);
+    loginUri.searchParams.append("response_type", "code");
+    loginUri.searchParams.append("scope", "openid email profile");
+    res.render("anonymous", { loginUri });
   } else {
     res.render("authenticated", {
       username: session.username,
-      attributes: session.attributes,
+      attributes: session.attributes
     });
   }
 });
@@ -42,46 +42,46 @@ app.post("/login", (req, res) => {
     firstName: "Daniel",
     lastName: "Garnier-Moiroux",
     company: "Broadcom",
-    userType: "hardcoded",
+    userType: "hardcoded"
   };
   res.redirect("/");
 });
 
-app.post('/logout', (req, res) => {
-  session = req.session
-  session.username = null
-  session.attributes = null
-  res.redirect('/')
-})
+app.post("/logout", (req, res) => {
+  session = req.session;
+  session.username = null;
+  session.attributes = null;
+  res.redirect("/");
+});
 
-app.get('/oauth2/callback', (req, res) => {
-  const code = req.query.code
-  console.log(code)
+app.get("/oauth2/callback", (req, res) => {
+  const code = req.query.code;
+  console.log(code);
   fetch("https://oauth2.googleapis.com/token", {
     method: "POST",
     body: new URLSearchParams({
       code,
       client_id,
       redirect_uri,
-      grant_type: "authorization_code",
+      grant_type: "authorization_code"
     }),
     headers: {
       authorization: `Basic ${btoa(client_id + ":" + client_secret)}`,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
   })
     .then(r => r.json())
     .then(r => {
-      const id_token = r.id_token
-      const payload = id_token.split(".")[1]
-      const parsed = JSON.parse(atob(payload))
-      console.log(parsed)
-      const session = req.session
-      session.username = parsed.email
-      session.attributes = parsed
-      res.redirect("/")
-    })
-})
+      const id_token = r.id_token;
+      const payload = id_token.split(".")[1];
+      const parsed = JSON.parse(atob(payload));
+      console.log(parsed);
+      const session = req.session;
+      session.username = parsed.email;
+      session.attributes = parsed;
+      res.redirect("/");
+    });
+});
 
 
 app.listen(3000, () => {
